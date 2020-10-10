e741428757d55d5d6f1400238b7a8cf70d7f399b
Yuri Toledo <yuriwtoledo@gmail.com>
2020-10-09 21:48:19 -0300

    refac: remove dead code

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 
 type Props = {
@@ -16,33 +15,7 @@ const Layout = (props: Props) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <header>
-        <nav>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          {' '}
-          |
-          {' '}
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-          {' '}
-          |
-          {' '}
-          <Link href="/users">
-            <a>Users List</a>
-          </Link>
-          {' '}
-          |
-          {' '}
-          <a href="/api/users">Users API</a>
-        </nav>
-      </header>
       {children}
-      <footer>
-        <hr />
-      </footer>
     </div>
   );
 };
