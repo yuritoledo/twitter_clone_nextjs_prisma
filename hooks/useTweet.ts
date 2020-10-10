cc7bb87e35af02c2a475397d6c4046f7533a1668
Yuri Toledo <yuriwtoledo@gmail.com>
2020-10-09 21:48:09 -0300

    feat: create useTweet hook

---
+++ hooks/useTweet.ts
@@ -0,0 +1,16 @@
+import { User } from '@prisma/client';
+import useSWR from 'swr';
+
+type Tweet = {
+  id: number
+  text: string
+  User: Pick<User, 'name'>
+}
+
+const useTweet = () => {
+  const { data, isValidating, error } = useSWR<Tweet[]>('/api/tweet');
+
+  return { data, isValidating, error };
+};
+
+export default useTweet;
