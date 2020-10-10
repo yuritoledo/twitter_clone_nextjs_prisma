b85f848dcbd2cb8b8c305a9427c62d282e07440f
Yuri Toledo <yuriwtoledo@gmail.com>
2020-10-09 21:51:29 -0300

    feat: Add tweet list in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,36 @@
+import { Tweet } from '@prisma/client';
 import Layout from 'components/Layout';
-import { createTweet } from 'services/tweet';
+import useTweet from 'hooks/useTweet';
 
-const IndexPage = () => {
-  const fill = () => {
-    createTweet({
-      text: 'alou',
-      User: { connect: { id: 1 } },
-    });
-  };
+// const fetcher = (...url: any) => fetch(url).then((x) => x.json());
+
+const TweetsList = () => {
+  const { data, isValidating, error } = useTweet();
+  console.log(data);
+
+  if (isValidating) return <span>aguarde ...</span>;
+  if (error) {
+    console.log(error);
+    return <span>ERROOOUU</span>;
+  }
 
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Stranger </h1>
-      <button type="button" onClick={fill}>aoou</button>
-    </Layout>
+    <ul>
+      {data?.map((t) => (
+        <li key={t.id}>
+          <span>{t.text}</span>
+          <strong>{t.User.name}</strong>
+        </li>
+      ))}
+    </ul>
   );
 };
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Hello Stranger, here is your feed</h1>
+    <TweetsList />
+  </Layout>
+);
 
 export default IndexPage;
