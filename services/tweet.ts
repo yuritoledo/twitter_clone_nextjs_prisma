4028f62fcba015774ec34453c13057a86de6d735
Yuri Toledo <yuriwtoledo@gmail.com>
2020-11-06 18:20:41 -0300

    feat: created tweet frontend service

--- services/tweet.ts
+++ services/tweet.ts
@@ -1,13 +1,28 @@
-import { TweetCreateInput } from '@prisma/client';
+import { Tweet, TweetCreateInput } from '@prisma/client'
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: '/api',
+})
 
 export const createTweet = async (tweet: TweetCreateInput) => {
-  const httpResponse = await fetch('api/tweets', {
-    method: 'POST',
-    body: JSON.stringify(tweet),
-  });
+  // const httpResponse = await fetch('api/tweet', {
+  //   method: 'POST',
+  //   body: JSON.stringify(tweet),
+  // })
 
-  const response = await httpResponse.json();
-  return response;
-};
+  // const response = await httpResponse.json()
+  // return response
+}
 
-export const fetchAllTweets = () => { };
+export const fetchAllTweets = () => { }
+
+export const deleteTweet = async (id: number): Promise<Tweet | null> => {
+  try {
+    const response = await api.delete<Tweet>(`/tweet/${id}`)
+    return response.data
+  } catch (error) {
+    console.log('error', error)
+    return null
+  }
+}
