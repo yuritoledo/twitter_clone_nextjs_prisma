821b9055fd6aa73b75bab57dd4e35502cc4be91d
Yuri Toledo <yuriwtoledo@gmail.com>
2020-11-06 18:20:54 -0300

    feat: created tweet repository

--- repositories/tweet.ts
+++ repositories/tweet.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, TweetUpdateInput } from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-const getAllTweetsWithUserName = () => prisma.tweet.findMany({
+export const getAllTweetsWithUserName = () => prisma.tweet.findMany({
   orderBy: {
     id: 'desc',
   },
@@ -13,6 +13,23 @@ const getAllTweetsWithUserName = () => prisma.tweet.findMany({
       },
     },
   },
-});
+})
 
-export default getAllTweetsWithUserName;
+export const findById = (id: number) => prisma.tweet.findOne({
+  where: {
+    id,
+  },
+})
+
+export const updateById = (data: TweetUpdateInput, id: number) => prisma.tweet.update({
+  data,
+  where: {
+    id,
+  },
+})
+
+export const remove = (id: number) => prisma.tweet.delete({
+  where: {
+    id,
+  },
+})
