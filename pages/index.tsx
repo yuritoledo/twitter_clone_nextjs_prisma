9429c9f894ea534306db3a7d6971bc31b3521bcf
Yuri Toledo <yuriwtoledo@gmail.com>
2020-11-06 18:20:01 -0300

    feat(WIP): updated main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,45 @@
-import { Tweet } from '@prisma/client';
-import Layout from 'components/Layout';
-import useTweet from 'hooks/useTweet';
+import { Flex, SimpleGrid } from '@chakra-ui/core'
+import Layout from 'components/Layout'
+import useTweet from 'hooks/useTweet'
+import Link from 'next/link'
 
 // const fetcher = (...url: any) => fetch(url).then((x) => x.json());
 
 const TweetsList = () => {
-  const { data, isValidating, error } = useTweet();
-  console.log(data);
+  const { data, isValidating, error } = useTweet()
 
-  if (isValidating) return <span>aguarde ...</span>;
+  if (isValidating) return <span>aguarde ...</span>
   if (error) {
-    console.log(error);
-    return <span>ERROOOUU</span>;
+    console.log(error)
+    return <span>ERROOOUU</span>
   }
 
   return (
-    <ul>
+    <SimpleGrid columns={[1, 3]} spacing="40px">
       {data?.map((t) => (
-        <li key={t.id}>
+        <Flex
+          key={t.id}
+          height="80px"
+          borderWidth="1px"
+          alignItems="center"
+          bg="yellow"
+        >
           <span>{t.text}</span>
           <strong>{t.User.name}</strong>
-        </li>
+        </Flex>
       ))}
-    </ul>
-  );
-};
+      <Link href="/meus_tweets">
+        <h1>Meus tweets</h1>
+      </Link>
+    </SimpleGrid>
+  )
+}
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Stranger, here is your feed</h1>
     <TweetsList />
   </Layout>
-);
+)
 
-export default IndexPage;
+export default IndexPage
